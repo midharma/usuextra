c65a931a6800a6adc04533ac5b1e6170682e9356
midharma <67436543+midharma@users.noreply.github.com>
2025-02-13 16:39:29 +0800

    Update fake.py

--- plugins/fake.py
+++ plugins/fake.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import filters
 
 from UsuMusic import app
-
+from UsuMusic.core.clone import usu
 
 @app.on_message(filters.command(["FAKE", "fake"]))
 async def fkadress(_, message):
